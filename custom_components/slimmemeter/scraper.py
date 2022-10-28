0d3c7ce51ff5a370a774f613897eae812b921927
JxxIT <110342008+JxxIT@users.noreply.github.com>
2022-10-28 17:42:50 +0200

    Remove unused imports.

--- custom_components/slimmemeter/scraper.py
+++ custom_components/slimmemeter/scraper.py
@@ -1,6 +1,4 @@
-import os
 import mechanize
-import xlrd
 from datetime import datetime, timedelta, time
 import time as tm
 
@@ -45,15 +43,14 @@ def gas_yesterday(email, password):
     midnight = datetime.combine(datetime.today(), time.min)
     yesterday_midnight = midnight - timedelta(days=1)
     timestamp = tm.mktime(yesterday_midnight.timetuple())
-    scrape(86400, timestamp, str(email), str(password), "power_yesterday", "gas")
+    scrape(86400, timestamp, str(email), str(password), "gas_yesterday", "gas")
 
 def gas_month(email, password):
     midnight = datetime.combine(datetime.today().replace(day=1), time.min)
     timestamp = tm.mktime(midnight.timetuple())
-    scrape(2629746, timestamp, str(email), str(password), "power_month", "gas")
+    scrape(2629746, timestamp, str(email), str(password), "gas_month", "gas")
 
 def gas_year(email, password):
     midnight = datetime.combine(datetime(datetime.today().year, 1, 1), time.min)
     timestamp = tm.mktime(midnight.timetuple())
-    scrape(31556952, timestamp, str(email), str(password), "power_year", "ga")
-gas_month("demo@slimmemeterportal.nl", "demo123")
+    scrape(31556952, timestamp, str(email), str(password), "gas_year", "ga")
